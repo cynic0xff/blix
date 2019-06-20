adaabe3c9cf488bc372edb14a3f45cc91d0dcb55
Scott Wren <cynics0xff@gmail.com>
2019-06-20 16:36:38 -0400

    Add backend API url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { ReactiveFormsModule } from '@angular/forms';
 import  { HttpClientModule } from '@angular/common/http';
+import { ProductsService } from './products.service';
 
 
 @NgModule({
@@ -25,7 +26,9 @@ import  { HttpClientModule } from '@angular/common/http';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ProductsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
