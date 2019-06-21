6709a2e5e97556ecb1b4de8860056ebae3463763
Scott Wren <cynics0xff@gmail.com>
2019-06-21 09:34:30 -0400

    Add routes for product edit, update, and delete. Update server.js to use products end point

--- src/api/routes/product.route.js
+++ src/api/routes/product.route.js
@@ -2,22 +2,67 @@ const express = require('express');
 const app = express();
 const productRoutes = express.Router();
 
-let product = require('../models/Product');
+// Require Product model in our routes module
+let Product = require('../models/Product');
 
-productRoutes.route('/add').post((req, res) => {
-    let product = new product(req.body);
-    product.save()
-        .then(product => {
-            res.status(200).json({'Product': 'Product has been added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send("Unable to save to database");
-        });
-});
-
-productRoutes.route('/').get((req, res) => {
-    let id = req.params.id;
-    Product.findById(id, (err, product) => {
-        res.json(product);
+// Defined store route
+productRoutes.route('/add').post(function (req, res) {
+  let product = new Product(req.body);
+  product.save()
+    .then(product => {
+      res.status(200).json({'Product': 'Product has been added successfully'});
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
     });
 });
+
+// Defined get data(index or listing) route
+productRoutes.route('/').get(function (req, res) {
+  Product.find(function (err, products){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.json(products);
+    }
+  });
+});
+
+// Defined edit route
+productRoutes.route('/edit/:id').get(function (req, res) {
+  let id = req.params.id;
+  Product.findById(id, function (err, product){
+      res.json(product);
+  });
+});
+
+//  Defined update route
+productRoutes.route('/update/:id').post(function (req, res) {
+  Product.findById(req.params.id, function(err, product) {
+    if (!product)
+      res.status(404).send("Record not found");
+    else {
+      product.ProductName = req.body.ProductName;
+      product.ProductDescription = req.body.ProductDescription;
+      product.ProductPrice = req.body.ProductPrice;
+
+      product.save().then(product => {
+          res.json('Update complete');
+      })
+      .catch(err => {
+            res.status(400).send("unable to update the database");
+      });
+    }
+  });
+});
+
+// Defined delete | remove | destroy route
+productRoutes.route('/delete/:id').get(function (req, res) {
+    Product.findByIdAndRemove({_id: req.params.id}, function(err, product){
+        if(err) res.json(err);
+        else res.json('Successfully removed');
+    });
+});
+
+module.exports = productRoutes;
--- src/api/server.js
+++ src/api/server.js
@@ -2,19 +2,22 @@ const express = require('express'),
     path = require('path'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
-    mongoose = require('mongoose');
-    config = require('./db');
+    mongoose = require('mongoose'),
+    config = require('./DB');
 
-    //make a connection to the database
+   const productRoute = require('./routes/product.route');
     mongoose.Promise = global.Promise;
-    mongoose.connect(config.DB, { useNewUrlParser: true })
-        .then(() => { console.log('Database connected')},
-        err => { console.log('Cannot connect to datbase. ' + err)}
+    mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+      () => {console.log('Database is connected') },
+      err => { console.log('Can not connect to the database'+ err)}
     );
 
     const app = express();
-    let port = process.env.PORT || 4000;
+    app.use(bodyParser.json());
+    app.use(cors());
+    app.use('/products', productRoute);
+    const port = process.env.PORT || 4000;
 
-    const server = app.listen(() => {
-        console.log(`Listening on port ${port}`);
+    const server = app.listen(port, function(){
+     console.log('Listening on port ' + port);
     });
