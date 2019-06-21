4262ac423c3aef7e5dcecea9f3851c0f6705e20b
Scott Wren <cynics0xff@gmail.com>
2019-06-21 06:42:57 -0400

    Add backend database connection to mlab

--- src/api/server.js
+++ src/api/server.js
@@ -3,6 +3,14 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
     mongoose = require('mongoose');
+    config = require('./db');
+
+    //make a connection to the database
+    mongoose.Promise = global.Promise;
+    mongoose.connect(config.DB, { useNewUrlParser: true })
+        .then(() => { console.log('Database connected')},
+        err => { console.log('Cannot connect to datbase. ' + err)}
+    );
 
     const app = express();
     let port = process.env.PORT || 4000;
@@ -10,4 +18,3 @@ const express = require('express'),
     const server = app.listen(() => {
         console.log(`Listening on port ${port}`);
     });
-    
